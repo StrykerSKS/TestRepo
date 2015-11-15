680d205d48afdbf986c59508894abe22cd6a7033
Sean Schneyer <sean@linuxbox.org>
2015-11-14 19:18:06 -0600

    Update and rename devicetypes/basicdirect.src/basicdirect.groovy to devicetypes/basic-direct.src/basic-direct.groovy

--- devicetypes/basic-direct.src/basic-direct.groovy
+++ devicetypes/basic-direct.src/basic-direct.groovy
@@ -27,9 +27,6 @@ preferences {
 metadata {
     // Automatically generated. Make future change here.
     definition (name: "Basic Direct", author: "Sean Schneyer") {
-        capability "Polling"
-        capability "Button"
-        capability "Refresh"
         capability "Alarm"
         command "arm"
         attribute "partition1", "string"
@@ -46,40 +43,14 @@ metadata {
     tiles {
         standardTile("button","device.mainState", width: 2, height: 2, canChangeIcon: true) {
             state "default", label: 'Default', action: "arm", icon: "st.Home.home2", backgroundColor: "#79b821", nextState: "arming"
-            state "disarm", label: 'Disarmed', action: "arm", icon: "st.Home.home2", backgroundColor: "#79b821", nextState: "arming"
             state "arm", label: 'Armed', action: "disarm", icon: "st.Home.home3", backgroundColor: "#b82078", nextState: "disarming"
-            state "alarming", label: 'ALARMING', action: "disarm", icon: "st.Home.home3", backgroundColor: "#b82078", nextState: "disarming"
-            state "disarming", label: 'Disarming', action: "arm", icon: "st.Home.home2", backgroundColor: "#b8ab20", nextState: "disarm"
-            state "arming", label: 'Arming', action: "disarm", icon: "st.Home.home3", backgroundColor: "#b8ab20", nextState: "arm"
-            state "night", label: 'Armed Night', action: "disarm", icon: "st.Home.home3", backgroundColor: "#2078b8", nextState: "disarming"
-
+ 
         }
         standardTile("nightbutton","device.button", width: 1, height: 1, canChangeIcon: true) {
             state "default", label: 'Night Arm', action: "nightarm", icon: "st.Weather.weather4", backgroundColor: "#2078b8", nextState: "default"            
         }
-        standardTile("alarmStatus","device.alarmStatus", width: 1, height: 1, canChangeIcon: true) {
-            state "ready", label: 'Ready', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "readyf", label: 'Ready - F', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "notready", label: 'Not Ready', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "alarming", label: 'ALARMING', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "entry", label: 'Entry Delay', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "exit", label: 'Exit Delay', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "lockout", label: 'Keypad Lockout', action: "f", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "fail", label: 'Failed to Arm', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "away", label: 'Armed - Away', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "stay", label: 'Armed - Stay', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "zeroaway", label: 'Zero Entry Away', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "zerostay", label: 'Zero Entry Stay', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""
-            state "disarmed", label: 'Disarmed', action: "", icon: "", backgroundColor: "#ffffff", nextState: ""            
-        }
-        standardTile("update", "default", inactiveLabel: false) {
-            state "default", action:"updatestatus", icon:"st.secondary.refresh"
-        }
-        standardTile("disarmbutton", "device.button", inactiveLabel:false) {
-            state "default", label:"Disarm", icon:"st.Home.home2", action:"disarm"
-        }
-        main (["button","nightbutton","alarmStatus","update","disarmbutton"])
-        details(["button","nightbutton","alarmStatus","update","disarmbutton"])
+        main (["button"])
+        details(["button"])
     }
 }
 
